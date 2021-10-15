84cf3f9b872eee54693804096b15de9bc01b00fe
toveseger <tove.seger@outlook.com>
2021-10-15 15:48:39 +0200

    More structure added in ProgramLogic

--- ProgramLogic.cs
+++ ProgramLogic.cs
@@ -5,28 +5,30 @@
     using System.IO;
     using System.Text;
 
-
-
     class ProgramLogic
     {
-        public List<string> textListOne { get; } = new List<string>();
+        public List<string> textListOne { get; set; } = new List<string>();
+        public List<string> textListTwo { get; set; } = new List<string>();
+        public List<string> textListThree { get; set; } = new List<string>();
         public string pathToFile1 { get; } = @"TextFiles\File1.txt";
+        public string pathToFile2 { get; } = @"TextFiles\File2.txt";
+        public string pathToFile3 { get; } = @"TextFiles\File2.txt";
 
 
 
-        //Reads file 
-        public void ReadFile(string filePath)
-        {
-            using (StreamReader sr = File.OpenText(filePath))
-            {
-                string s;
-                while ((s = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine(s);
-                }
-            }
+        ////Reads file 
+        //public void ReadFile(string filePath)
+        //{
+        //    using (StreamReader sr = File.OpenText(filePath))
+        //    {
+        //        string s;
+        //        while ((s = sr.ReadLine()) != null)
+        //        {
+        //            Console.WriteLine(s);
+        //        }
+        //    }
 
-        }
+        //}
 
         public List<string> AddFileToList(string filePath, List<string> stringList)
         {
@@ -40,6 +42,7 @@
 
                     foreach (var w in words)
                     {
+                        w.ToLower();
                         stringList.Add(w);
                     }          
                 }
@@ -51,6 +54,32 @@
         {
             if (list.Contains(value)) return true;
             else return false;
+        }
+
+        public void CheckValueInDocuments(string value)
+        {
+             textListOne = AddFileToList(pathToFile1, textListOne);
+             textListTwo = AddFileToList(pathToFile2, textListTwo);
+             textListThree = AddFileToList(pathToFile3, textListThree);
+
+            var listOneContainsValue = CheckValueInList(value, textListOne);
+            int counterListOne=0;
+
+            foreach (var item in textListOne)
+            {
+                Console.WriteLine(item);
+            }
+            //if (listOneContainsValue)
+            //{
+            //    foreach (var i in textListOne)
+            //    {
+            //        if (i == value) counterListOne++;
+            //    }
+            //    Console.WriteLine($"{value} exists {counterListOne} times in text one");
+            //}
+
+            var listTwoContainsValue = CheckValueInList(value, textListTwo);
+            var listThreeContainsValue = CheckValueInList(value, textListThree);
         }
     }
 }
