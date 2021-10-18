4adc303401cf7cf54a5dc7b6f9d3301afd250d1b
toveseger <tove.seger@outlook.com>
2021-10-18 10:23:04 +0200

    Highest occurence added

--- ProgramLogic.cs
+++ ProgramLogic.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Text;
 
     class ProgramLogic
@@ -12,23 +13,11 @@
         public List<string> textListThree { get; set; } = new List<string>();
         public string pathToFile1 { get; } = @"TextFiles\File1.txt";
         public string pathToFile2 { get; } = @"TextFiles\File2.txt";
-        public string pathToFile3 { get; } = @"TextFiles\File2.txt";
+        public string pathToFile3 { get; } = @"TextFiles\File3.txt";
 
-        //HEJHEJ
-
-        ////Reads file 
-        //public void ReadFile(string filePath)
-        //{
-        //    using (StreamReader sr = File.OpenText(filePath))
-        //    {
-        //        string s;
-        //        while ((s = sr.ReadLine()) != null)
-        //        {
-        //            Console.WriteLine(s);
-        //        }
-        //    }
-
-        //}
+        internal int counterListOne { get; set; } = 0;
+        internal int counterListTwo { get; set; } = 0;
+        internal int counterListThree { get; set; } = 0;
 
         public List<string> AddFileToList(string filePath, List<string> stringList)
         {
@@ -42,8 +31,8 @@
 
                     foreach (var w in words)
                     {
-                        w.ToLower();
-                        stringList.Add(w);
+                        string word = w.ToLower();                       
+                        stringList.Add(word);
                     }          
                 }
             }
@@ -63,23 +52,66 @@
              textListThree = AddFileToList(pathToFile3, textListThree);
 
             var listOneContainsValue = CheckValueInList(value, textListOne);
-            int counterListOne=0;
+            //int counterListOne=0;
+            //int counterListTwo = 0;
+            //int counterListThree = 0;
 
-            foreach (var item in textListOne)
+            foreach (var i in textListOne.Where(x => x == value))
             {
-                Console.WriteLine(item);
+                counterListOne++;
             }
-            //if (listOneContainsValue)
-            //{
-            //    foreach (var i in textListOne)
-            //    {
-            //        if (i == value) counterListOne++;
-            //    }
-            //    Console.WriteLine($"{value} exists {counterListOne} times in text one");
-            //}
+            foreach (var i in textListTwo.Where(x => x == value))
+            {
+                counterListTwo++;
+            }
+            foreach (var i in textListThree.Where(x => x == value))
+            {
+                counterListThree++;
+            }
+
+            Console.WriteLine($"{value} exists {counterListOne} times in text one");
+            Console.WriteLine($"{value} exists {counterListTwo} times in text two");
+            Console.WriteLine($"{value} exists {counterListThree} times in text three");
+           
 
             var listTwoContainsValue = CheckValueInList(value, textListTwo);
             var listThreeContainsValue = CheckValueInList(value, textListThree);
         }
+
+        public void CheckHighestOccurence()
+        {
+            Dictionary<string, int> Occurrence = new Dictionary<string, int>();
+            Occurrence.Add("List One", counterListOne);
+            Occurrence.Add("List Two", counterListTwo);
+            Occurrence.Add("List Three", counterListThree);
+
+            var sortedOccurence = from entry in Occurrence orderby entry.Value descending select entry;
+            Console.WriteLine("Ranking:");
+            foreach (KeyValuePair<string, int> entry in sortedOccurence)
+            {
+                Console.WriteLine(entry.Key);
+            }
+
+        }
     }
 }
+
+
+
+
+
+
+
+////Reads file 
+//public void ReadFile(string filePath)
+//{
+//    using (StreamReader sr = File.OpenText(filePath))
+//    {
+//        string s;
+//        while ((s = sr.ReadLine()) != null)
+//        {
+//            Console.WriteLine(s);
+//        }
+//    }
+
+//}
